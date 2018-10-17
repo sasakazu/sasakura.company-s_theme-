9f0002a71b028e8f2211edddcce202d19fe300a1
sasakazu <sasakura.company@gmail.com>
2018-10-17 12:52:19 +0900

    change...

---
+++ wp-content/themes/siteorigin-unwind/inc/contributors.php
@@ -0,0 +1,59 @@
+<?php
+return json_decode( '{
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 8834,
+		"score": 271.56916698816474,
+		"percent": 59.39281110113313
+	},
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 4648,
+		"score": 170.15879631199485,
+		"percent": 37.21412617874597
+	},
+	"d1bea7e3f82e561c135bf5a3d4f9f896": {
+		"name": "SiteOrigin Support",
+		"email": "d1bea7e3f82e561c135bf5a3d4f9f896",
+		"loc": 213,
+		"score": 11.163409793454717,
+		"percent": 2.441463795247753
+	},
+	"d203744279876c5eda043108e2611648": {
+		"name": "Alex S",
+		"email": "d203744279876c5eda043108e2611648",
+		"loc": 39,
+		"score": 2.4529915628075107,
+		"percent": 0.5364749840280989
+	},
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 13,
+		"score": 1.4021093939208937,
+		"percent": 0.3066445992372069
+	},
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 42,
+		"score": 0.31081718003732944,
+		"percent": 0.06797644322320476
+	},
+	"43c0e7220c23bd6636cf5469be6e9612": {
+		"name": "Braam Genis",
+		"email": "43c0e7220c23bd6636cf5469be6e9612",
+		"loc": 7,
+		"score": 0.18341291270759058,
+		"percent": 0.040112832390966136
+	},
+	"367746dddf905c98fc440395f0fc97f1": {
+		"name": "Rasmus Jürs",
+		"email": "367746dddf905c98fc440395f0fc97f1",
+		"loc": 1,
+		"score": 0.001783547453171456,
+		"percent": 0.00039006599368747886
+	}
+}', true );
